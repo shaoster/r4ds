1f91fce2f1f87b8d449b91ad3f788fa734e65245
Phil Shao <shaoster@gmail.com>
2025-12-30 23:36:29 +0000

    Reorganize files and start ML work

--- R for Data Science/1.R
+++ R for Data Science/1.R
@@ -5,6 +5,27 @@ library(plotly)
 
 glimpse(penguins)
 
+avg_bill_length <- mean(penguins$bill_length_mm, na.rm=TRUE)
+
+ggplot(
+  data = penguins,
+  mapping = aes(x = flipper_length_mm, y = body_mass_g)
+) + 
+  geom_point(mapping = aes(color = species, shape = species)) + 
+  geom_smooth(method = "lm") + 
+  labs(
+    title = "Body mass and flipper length",
+    subtitle = "Dimensions for Adelie, Chinstrap, and Gentoo Penguins",
+    x = "Flipper length (mm)", y = "Body m",
+    color = "Sp", shape = "Sp"
+  ) +
+  scale_color_colorblind()
+
+
+ggplotly(
+  p = ggplot2::last_plot()
+) 
+
 plot <- ggplot(
   data = penguins,
   mapping = aes(x = flipper_length_mm, y = body_mass_g)
@@ -20,3 +41,4 @@ plot <- ggplot(
   scale_color_colorblind()
 
 ggplotly(plot)
+ 
